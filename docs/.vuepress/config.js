e3e23ec6c520e23b3b3e581d8db9b219fdfbec84
“Genemator <genemator@icloud.com>
2019-05-31 21:13:31 +0500

    Changed everything

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,73 @@
 module.exports = {
-  title: 'Vuepress',
-  description: 'This is a Zeit Now 2.0 example',
+  title: 'UzHackintosh',
+  description: 'UzHackintosh - Hackintosh Guide for Uz Developers',
+  theme: 'yuu',
   themeConfig: {
+    yuu: {
+      colorThemes: ['blue', 'red'],
+    },
+    repo: 'Genemator/uzhackintosh',
+    docsDir: 'pages',
+    editLinks: true,
+    sidebarDepth: 3,
+    lastUpdated: true,
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'Config Page', link: '/config' },
-    ]
-  }
-}
+      {
+        text: 'Home',
+        link: '/',
+      },
+      {
+        text: 'Main Guide',
+        link: '/more-text',
+      },
+      {
+        text: 'Commando',
+        link: '/commando/',
+      },
+      {
+        text: 'Red',
+        link: '/red/',
+      },
+      {
+        text: 'Telegram',
+        link: 'https://t.me/uzhackintosh',
+      },
+    ],
+    sidebar: {
+      '/commando/': [
+        '',
+        'more-content',
+        'final-content',
+      ],
+      '/red/': [
+        '',
+        'more-content',
+        'final-content',
+      ],
+      '/': [
+        {
+          title: 'Home',
+          collapsable: false,
+          children: ['/', 'custom/'],
+        },
+        {
+          title: 'Markdown stuff',
+          collapsable: false,
+          children: [
+            'more-text',
+            'markdown',
+            'markdown-vuepress',
+            'markdown-it',
+          ],
+        },
+        {
+          title: 'Misc pages',
+          children: [
+            'custom-themes',
+            'default-theme-config',
+          ],
+        },
+      ],
+    },
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,14 @@
-# Hello From Vuepress on Now 2.0!
+---
+home: true
+heroImage: https://i.imgur.com/x61tQnz.png
+actionText: Get Started →
+actionLink: https://t.me/uzhackintosh
+features:
+- title: Simplicity First
+  details: Minimal setup with markdown-centered project structure helps you focus on writing.
+- title: Vue-Powered
+  details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
+- title: Performant
+  details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+footer: Copyright © 2019 The UzAnimation Team 
+---
